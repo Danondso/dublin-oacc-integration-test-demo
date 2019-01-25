aa894b9d18690b5d1ddbc62944cc02d7a010bb93
Danondso <Stratophone2112@gmail.com>
2019-01-25 08:21:20 -0600

    Initial commit for Oacc Integration Test Setup.

---
+++ src/main/java/com/dublin/oacc/demo/Application.java
@@ -0,0 +1,35 @@
+package com.dublin.oacc.demo;
+
+import com.acciente.oacc.*;
+import com.acciente.oacc.encryptor.bcrypt.BCryptPasswordEncryptor;
+import com.acciente.oacc.sql.SQLAccessControlContextFactory;
+import com.acciente.oacc.sql.SQLAccessControlSystemInitializer;
+import com.acciente.oacc.sql.SQLProfile;
+import com.acciente.oacc.sql.internal.persister.SQLConnection;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.acciente.oacc.sql.SQLAccessControlSystemInitializer.initializeOACC;
+
+@SpringBootApplication
+public class Application {
+
+    @Bean
+    public AccessControlContext createAccessControlContext(@Value("${spring.datasource.url}") String url, @Value("${spring.datasource.oacc.rootpassword}") String rootpassword) throws SQLException {
+        initializeOACC(DriverManager.getConnection(url), "OACC", rootpassword.toCharArray(), BCryptPasswordEncryptor.newInstance(12));
+        final AccessControlContext accessControlContext = SQLAccessControlContextFactory.getAccessControlContext(DriverManager.getConnection(url), "OACC", SQLProfile.SQLServer_12_0_NON_RECURSIVE, BCryptPasswordEncryptor.newInstance(12));
+        accessControlContext.authenticate(Resources.getInstance(0), PasswordCredentials.newInstance(rootpassword.toCharArray()));
+        return accessControlContext;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
